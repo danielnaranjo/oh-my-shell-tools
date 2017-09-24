1cb500810fae2ada58c73349713d50a4510c7cfc
Daniel Naranjo <dnaranjo@gmail.com>
2017-09-24 11:38:58 -0300

    new project

---
+++ newLaravel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+composer create-project --prefer-dist laravel/laravel $1
+cd $1
+composer install
+yarn install
+touch README.md
+cp .env.example .env
+git init
+git add -A
+git commit -m 'Initial commit'
+php artisan key:generate
+php artisan config:clear
+php artisan config:cache
